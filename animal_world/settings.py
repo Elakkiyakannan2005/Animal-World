117c2fa8996c75e7b8ca2797542db4f7bbd39be1
Portfolio <elakkiyakannan01062005@gmail.com>
2026-09-14 19:06:04 +0530

    Fix CSS static files

--- animal_world/settings.py
+++ animal_world/settings.py
@@ -33,12 +33,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'animal_world.urls'
@@ -99,6 +101,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Where `collectstatic` gathers files for production deployment.
 STATIC_ROOT = BASE_DIR / 'staticfiles'
--- animal_world/urls.py
+++ animal_world/urls.py
@@ -5,3 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('animals.urls')),
 ]
+from django.conf import settings
+from django.conf.urls.static import static
